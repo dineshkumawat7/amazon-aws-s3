53cb5dd44c0fe0b1a6b8f7202c9c784de6854ae4
Dinesh Kumawat <dkumawat7627@gmail.com>
2025-10-25 13:21:58 +0530

    initial commit

---
+++ src/test/java/com/aws/service/s3/AmazonAwsS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.service.s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonAwsS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
